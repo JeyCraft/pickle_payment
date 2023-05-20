6b3ece37b9b40dcd6c634e71f1adb1d09e0eaf31
Jey <63811758+JeyCraft@users.noreply.github.com>
2023-05-20 09:43:48 +0200

    de.lua
    German Translations

---
+++ locales/translations/de.lua
@@ -0,0 +1,14 @@
+Language["de"] = {
+    open_register = "Drücke ~INPUT_CONTEXT~ to open the register.",
+    register_title = "%s (Billing: %s)",
+    register_select_player = "Select Player to Bill.",
+    register_group_denied = "Du kannst diese Kasse nicht benutzen.",
+    cant_afford_target = "You cannot afford the bill you were issued. (Clerk ID: %s)",
+    cant_afford_source = "The person you have billed cannot afford this bill. (ID: %s)",
+    success_target = "You have paid your bill of $%s by %s. (Clerk ID: %s)",
+    success_source = "You have been paid your issued bill of $%s by %s. (ID: %s)",
+    bill_reject_target = "You have rejected your issued bill of $%s. (Clerk ID: %s)",
+    bill_reject_source = "Your issued bill of $%s was rejected by the customer. (ID: %s)",
+    payment_method_money = "Bar",
+    payment_method_bank = "Karte"
+}
